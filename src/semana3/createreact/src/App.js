5c17097213c5e95b4ac1f8066db747f31834bfce
cristian tech devops <cristiand.munozt@outlook.com>
2022-10-26 18:56:42 -0500

    semana 3 sp-2022

---
+++ src/semana3/createreact/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey <code>src/App.js</code> aCongratulation ..!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hola cristian
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
