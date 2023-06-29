e9cb4e35e0c0af3256b0989966a60841f0489594
Cristian Muñoz <cristiand.munozt@outlook.com>
2023-06-28 22:02:19 -0500

    Week 1 - Fundamentos de Node JS y OOP

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
-const express = require('express');
-const { faker } = require('@faker-js/faker');
-const apiRouter = require('./src/routes')
-
-const app = express();
-const port = 3001;
-app.use(express.json());
-app.get('/', (req, res) => {
-  res.send("Hola mundo");
-})
+// Variables de entorno
+let tokenApi = process.env.APITOKEN || "sin Token";
+let tajeta = process.env.TARJETA || "sin Token";
 
 
-try {
-  apiRouter(app);
-} catch (e) {
-  console.error(e.message);
+myBool = false;
+console.log(tokenApi)
+console.log(`My bool is: ${myBool ? "Aprobado": "Reprobado"}`) //If en una sola linea Ternarios
+
+//OOP
+class Date{
+
+  constructor(year, month, day){
+    this._year = this.validateYear(year);
+    this._month = this.validateMonth(month);
+    this._day = this.validateDay(day);
+  }
+  validateYear(year){
+    if(year < 0){
+      throw new Error("This year is not valid");
+    }
+    return year;
+  }
+  validateMonth(month){
+    if(month < 1 || month > 12){
+      throw new Error("This month is not valid");
+    }
+    return month;
+  }
+
+  validateDay(day){
+
+   if (day == 29 && this._month ==2 && this.isLeapYear(this._year)) {
+    return day
+   }
+
+   // Validar Los demas meses
+
+   if((day >= 1 && day <= 28 && this.month == 2) ||
+   (day >= 1 && day <= 30 && this.month == 4)){
+
+   }
+
+   throw new Error("This day is not valid");
+  }
+
+  isLeapYear(year){
+    return year % 400 == 0 || year % 4 == 0 && year % 100 != 0
+  }
+
+  Print(){
+    return `${this._year}/${this._month}/${this._day}`;
+  }
+
+
 }
-console.log("Hola");
-app.listen(port, () => {
-  console.log(`Conectado al puerto ${port}`);
-});
+
+// let fecha1 = new Date(2023,5,4);
+let fecha2 = new Date(2028,2,29);
+
+
+console.log(fecha2.Print());
 
